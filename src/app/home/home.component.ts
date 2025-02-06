c32e3a41117e847d9aabb98b5a45e78ff7a4aa20
harshsonar <harshsonar40@gmail.com>
2025-02-06 23:18:58 +0530

    adding very basic chat UI using NbChatModule, adjusted basic styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,21 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
 import { ButtonModule } from 'primeng/button';
 import { RouterService } from '../shared/router.service';
-import { NbLayoutModule, NbListModule, NbUserModule } from '@nebular/theme';
+import { NbLayoutModule, NbListModule, NbUserModule, NbChatModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, NbLayoutModule, NbListModule, NbUserModule],
+  imports: [ButtonModule, NbLayoutModule, NbListModule, NbUserModule, NbChatModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public authService: AuthService, private routerService: RouterService) { }
+  messages: any = [];
 
-  ngOnInit(): void {}
-
+  
   currentUserUsername: string | null = null;
   users: { name: string, title: string }[] = [
     { name: 'Carla Espinosa', title: 'Nurse' },
@@ -25,15 +24,17 @@ export class HomeComponent implements OnInit {
     { name: 'Perry Cox', title: 'Doctor of Medicine' },
     { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
   ];
+  
+  constructor(public authService: AuthService, private routerService: RouterService) { }
+  ngOnInit(): void {}
+  
+  sendMessage(event: any): void {}
 
   logout() {
     let result = false;
-
     result = window.confirm("Are you sure you want to logout?");
-
     if (result) {
       this.routerService.routeToLandingPage();
     }
-
   }
 }
